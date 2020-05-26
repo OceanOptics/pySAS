58ee4abca188516d1ed3e7e18e6353112f3218af
Jason Morrill <jason.morrill@maine.edu>
2020-05-26 15:33:16 -0400

    testingProcedure correctly points to pysas_cfg.ini

--- tests/testingProcedure.py
+++ tests/testingProcedure.py
@@ -1,5 +1,6 @@
 import configparser
 import os
+import time
 from pySAS.interfaces import IndexingTable, GPS, HyperSAS
 
 
@@ -11,23 +12,26 @@ class TestingProcedure:
         # Controllers & Sensors
         self.gps = GPS(self.cfg)
         self.indexing_table = IndexingTable(self.cfg)
-        self.hypersas = HyperSAS(self.cfg)
+        #self.hypersas = HyperSAS(self.cfg)
 
     def start_sensors(self):
         self.gps.start()
         self.gps.start_logging()
         self.indexing_table.start()
-        self.hypersas.start()
+        #self.hypersas.start()
 
     def run(self):
         self.start_sensors()
         for i in range(10):
-            for j in range(-180, 181):
+            for j in range(-180, 181, 4):
                 self.indexing_table.set_position(j, check_stall_flag=True)
+                #time.sleep(5)
+
 
 
 if __name__ == "__main__":
-    cfg_path = os.path.join(os.path.dirname(__file__), '..', 'pysas_cfg.ini')
+    os.chdir('..')
+    cfg_path = os.path.join(os.getcwd(), 'pySAS/pysas_cfg.ini')
     tp = TestingProcedure(cfg_path)
     tp.run()
 
